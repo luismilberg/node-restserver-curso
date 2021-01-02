c8ade513f74ba82eb87033e2a26437226d4a60fd
Luis Milberg <luismilberg@pop-os.localdomain>
2021-01-02 13:37:10 -0300

    Correción en nombre de colección

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:lYWXSL2SDrT24jNF@cluster0.rakzk.mongodb.net/<dbname>?retryWrites=true&w=majority';
+    urlDB = 'mongodb+srv://cafe-user:lYWXSL2SDrT24jNF@cluster0.rakzk.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
 process.env.URLDB = urlDB;
