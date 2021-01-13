a1b419d1b6f73fbe5d84624f856a1896f60f12fd
Luis Milberg <luismilberg@gmail.com>
2021-01-13 12:26:43 -0300

    Avance ABM imagenes

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
 
 //Default options
 app.use(fileUpload({useTempFiles: true}));
@@ -64,7 +65,7 @@ app.put('/upload/:tipo/:id', (req, res) => {
     //Guarda el archivo subido en el folder correspondiente
 
     //Cambiar el nombre al archivo
-    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
 
     archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
 
@@ -77,8 +78,22 @@ app.put('/upload/:tipo/:id', (req, res) => {
 
         //En este punto, imagen ya cargada
 
+        switch (tipo) {
+            case 'usuarios':
+                
+                imagenUsuario(id, res, nombreArchivo);
+                
+                break;
+            case 'productos':
+            
+                imagenProducto(id, res, nombreArchivo);
+                
+                break;
 
-        imagenUsuario(id, res, nombreArchivo);
+            default:
+                break;
+        }
+
 
     });
 
@@ -133,8 +148,56 @@ function imagenUsuario(id, res, nombreArchivo){
 
 }
 
-function imagenProducto(){
-    
+function imagenProducto(id, res, nombreArchivo){
+
+    Producto.findById(id, (err, productoDB) => {
+
+        //Verificamos si existe un error en la petición a la BD
+        if(err){
+            //Borramos la imagen cargada en el servidor
+            borraArchivo(nombreArchivo, 'productos');
+
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        //Verificamos que el producto exista en la BD
+        if(!productoDB){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'El producto buscado no existe'
+                }
+            });
+        }
+
+        //Pasadas las validaciones, elimintamos la imágen anterior del servidor
+        borraArchivo(productoDB.img, 'productos');
+        console.log(productoDB);
+
+        //Reemplazamos la imagen en el producto a guardar
+        productoDB.img = nombreArchivo;
+
+        //Guardamos el producto actualizado en la base de datos
+
+        productoDB.save((err, productoGuardado) => {
+            if(err){
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            });
+        });
+
+
+    });
 }
 
 function borraArchivo(nombreImagen, tipo){
